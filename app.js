40a751a934a346ba0d72357921324f5713393d2d
joshhawks1823 <jheat23@gmail.com>
2019-11-26 11:36:37 -0500

    final commit

--- app.js
+++ app.js
@@ -3,21 +3,24 @@ document.addEventListener("DOMContentLoaded", gif);
 function gif() {
   document.getElementById("btnSearch").addEventListener("click", ev => {
     ev.preventDefault();
-    let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=25&q=`;
     let str = document.getElementById("search").value.trim();
     url = url.concat(str);
     console.log(url);
     fetch(url)
       .then(response => response.json())
       .then(content => {
+        console.log(content.data); // Retrieves actual Data
+        console.log("META", content.meta); // Retrieves Meta Data
         let fig = document.createElement("figure");
+        let h = document.createElement("h3");
         let img = document.createElement("img");
-        let fc = document.createElement("figcaption");
         img.src = content.data[0].images.downsized.url;
-        fc.textContent = content.data[0].title;
-        fig.appendChild(img);
-        fig.appendChild(fc);
-        let out = document.querySelector(".out");
+        h.textContent = content.data[0].title;
+        fig.appendChild(h);
+        fig.appendChild(img).width = "250";
+
+        let out = document.querySelector(".output");
         out.insertAdjacentElement("afterbegin", fig);
         document.querySelector("#search").value = "";
       })
